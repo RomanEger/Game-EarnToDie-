8d39a6c5d47008ea84cfa429890da3cf8fa2e01a
RomanEger <egerromaan@gmail.com>
2022-07-06 01:53:48 +0600

    commit

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed;
 
-    // Update is called once per frame
+    public float MaxSpeed;
+
+    public float speedUp;
+
+    public int MotorPower = 10000;
+
+    public WheelJoint2D[] Wheels = new WheelJoint2D[2];
+
+    JointMotor2D Motor;
+
     void Update()
     {
+        if (Input.GetAxisRaw("Horizontal")>0 && Motor.motorSpeed < MaxSpeed)
+        {
+            Motor.motorSpeed = (Motor.motorSpeed + speedUp) + Time.deltaTime;
+            
+            Motor.maxMotorTorque = MotorPower;
+
+            Wheels[0].motor = Motor;
+            Wheels[1].motor = Motor;
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0 && Motor.motorSpeed > MaxSpeed)
+        {
+            Motor.motorSpeed = (Motor.motorSpeed - speedUp) - Time.deltaTime;
+
+            Motor.maxMotorTorque = MotorPower;
+
+            Wheels[0].motor = Motor;
+            Wheels[1].motor = Motor;
+        }
+        else
+        {
+            if (Motor.motorSpeed > 0)
+            {
+                Motor.motorSpeed = (Motor.motorSpeed - speedUp) - Time.deltaTime;
+            }
+            else if (Motor.motorSpeed < 0)
+            {
+                Motor.motorSpeed = (Motor.motorSpeed + speedUp) + Time.deltaTime;
+            }
+        }
         
+        
+        /*if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }*/
     }
 }
